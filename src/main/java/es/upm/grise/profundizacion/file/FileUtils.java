afd8fb6ce25801f059349487a688d92c15628c5a
Alejandro <alejandro.albaladejo@alumnos.upm.es>
2025-12-03 19:58:50 +0100

    clase FileUtils actualizada

--- src/main/java/es/upm/grise/profundizacion/file/FileUtils.java
+++ src/main/java/es/upm/grise/profundizacion/file/FileUtils.java
@@ -1,10 +1,17 @@
 package es.upm.grise.profundizacion.file;
+import java.util.zip.CRC32;
 
 public class FileUtils {
 	
-	long calculateCRC32(byte[] bytes) {
-		
-		return 0L;
+	public static long calculateCRC32(byte[] bytes) {
+        CRC32 crc = new CRC32();
+
+        for(byte b : bytes) {
+            crc.update(b);
+        }
+        
+        return crc.getValue();
+
 		
 	}
 
